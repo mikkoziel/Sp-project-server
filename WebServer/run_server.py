b41fb98cf8340dde3456324140e68ba3dccf9328
mikkoziel <mkoziel045@gmail.com>
2020-06-17 23:11:33 +0200

    Port fun

--- WebServer/run_server.py
+++ WebServer/run_server.py
@@ -1,5 +1,5 @@
 from waitress import serve
 import web_server
 
-port_ = process.env.PORT || 5000
+port_ = process.env.PORT if process.env.PORT else 5000
 serve(web_server.app, host='0.0.0.0', port=port_)
